618350b8318d3ef61cea3ba7a20252fb3cf50b83
yamamoto takumi <takumi.yamamoto@ow-japan.com>
2020-12-24 01:46:35 +0900

    3章完了

---
+++ 3/practice.rb
@@ -0,0 +1,12 @@
+def fizz_buzz(number)
+  return number.to_s if number == 0
+  if number % 15 == 0
+    "Fizz Buzz"
+  elsif number % 5 == 0
+    "Buzz"
+  elsif number % 3 == 0
+    "Fizz"
+  else
+    number.to_s
+  end
+end
